447e4a4463ea9ce1a4771b4ca7930fe839ea33cf
gittiggit <contactwithmahmud@gmail.com>
2015-11-13 16:46:57 +0600

    Commited after resolving conflicts in the 45002_signup branch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112121102) do
+ActiveRecord::Schema.define(version: 20151113033636) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "target_id"
