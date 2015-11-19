cdb38523fa6a399677e66f62269de8beae2a4f4a
Manna, Manazir Ahsan <manazirahsan.developer@gmail.com>
2015-11-19 11:44:54 +0600

    45004_user_management

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate page: params[:page], per_page: 10
+  end
+
+  def show
+    @user = User.find params[:id]
+  end
+
   def new
     @user = User.new
   end
@@ -6,22 +18,54 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      flash[:success] = t :hello
       log_in @user
+      flash[:success] = t :hello
       redirect_to @user
     else
-      flash.now[:danger] = t :login_error
       render 'new'
     end
   end
 
-  def show
+  def edit
     @user = User.find params[:id]
   end
 
+  def update
+    @user = User.find params[:id]
+    if @user.update_attributes user_params
+      flash[:success] = t :users_update_success_text
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = t :users_destroy_success_text
+    redirect_to users_url
+  end
+
   private
+
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = t :login_request_text
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find params[:id]
+    redirect_to(root_url) unless @user == current_user
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
